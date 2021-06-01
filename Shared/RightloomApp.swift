94ff80b06a9b28aeaedcfa8d5c9fc8a25d5cd6ee
kawabata.satoshi <satoshi.kawa8ata@gmail.com>
2021-06-02 00:00:14 +0900

    Initial Commit

---
+++ Shared/RightloomApp.swift
@@ -0,0 +1,17 @@
+//
+//  RightloomApp.swift
+//  Shared
+//
+//  Created by Satoshi Kawabata on 2021/06/02.
+//
+
+import SwiftUI
+
+@main
+struct RightloomApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
