bd5e66a16b48a260bbfdd936c434fdc8aae453fe
kawabata.satoshi <satoshi.kawa8ata@gmail.com>
2021-06-22 01:36:23 +0900

    added upload feature from image picker

--- Shared/Views/PhotoGridView.swift
+++ Shared/Views/PhotoGridView.swift
@@ -5,6 +5,48 @@
 
 import SwiftUI
 
+func uploadImage(token: String, paramName: String, fileName: String, image: UIImage, completion: @escaping (Error?)->Void) {
+    let url = URL(string: "http://localhost/api/photos/")
+
+    // generate boundary string using a unique per-app string
+    let boundary = UUID().uuidString
+
+    let session = URLSession.shared
+
+    // Set the URLRequest to POST and to the specified URL
+    var urlRequest = URLRequest(url: url!)
+    urlRequest.httpMethod = "POST"
+
+    urlRequest.addValue("Bearer " + token, forHTTPHeaderField: "Authorization")
+
+    // Set Content-Type Header to multipart/form-data, this is equivalent to submitting form data with file upload in a web browser
+    // And the boundary is also set here
+    urlRequest.setValue("multipart/form-data; boundary=\(boundary)", forHTTPHeaderField: "Content-Type")
+
+    var data = Data()
+
+    // Add the image data to the raw http request data
+    data.append("\r\n--\(boundary)\r\n".data(using: .utf8)!)
+    data.append("Content-Disposition: form-data; name=\"\(paramName)\"; filename=\"\(fileName)\"\r\n".data(using: .utf8)!)
+    data.append("Content-Type: image/jpeg\r\n\r\n".data(using: .utf8)!)
+    data.append(image.jpegData(compressionQuality: 1)!)
+
+    data.append("\r\n--\(boundary)--\r\n".data(using: .utf8)!)
+
+    // Send a POST request to the URL, with the data we created earlier
+    session.uploadTask(with: urlRequest, from: data, completionHandler: { responseData, response, error in
+        if error == nil {
+            let jsonData = try? JSONSerialization.jsonObject(with: responseData!, options: .allowFragments)
+            if let json = jsonData as? [String: Any] {
+                print(json)
+            }
+        }
+        DispatchQueue.main.async {
+            completion(error)
+        }
+    }).resume()
+}
+
 func downloadImageAsync(url: URL, completion: @escaping (UIImage?) -> Void) {
     let session = URLSession(configuration: .default)
     let task = session.dataTask(with: url) { (data, _, _) in
@@ -19,7 +61,6 @@ func downloadImageAsync(url: URL, completion: @escaping (UIImage?) -> Void) {
     task.resume()
 }
 
-
 struct PhotoGridView: View {
     @State var photos = [Photo]()
     @State var images = [UIImage?]()
@@ -27,6 +68,9 @@ struct PhotoGridView: View {
     @State var navigationViewIsActive = false
     @State var selectedPhoto: Photo? = nil
     
+    @State var isShowPhotoLibrary = false
+    @State var pickedImage = UIImage()
+
     @EnvironmentObject var authInfo: AuthInfo
     @EnvironmentObject var settings: Settings
     
@@ -83,11 +127,35 @@ struct PhotoGridView: View {
                 fetchPhotos(urlString: url)
             })
         }
+        .sheet(isPresented: $isShowPhotoLibrary, onDismiss: {
+            // upload image
+            uploadImage(token: authInfo.token, paramName: "file", fileName: "aaa.jpeg", image: self.pickedImage) { error in
+                if error == nil {
+                    let url = settings.serverURL + "/api/photos"
+                    fetchPhotos(urlString: url)
+                }
+            }
+
+        }, content: {
+            ImagePicker(sourceType: .photoLibrary, selectedImage:  self.$pickedImage)
+        })
+        .toolbar {
+            ToolbarItemGroup(placement: .bottomBar) {
+                Spacer()
+                Button(action: {
+                    isShowPhotoLibrary = true
+                }) {
+                    Image(systemName: "square.and.arrow.up")
+                }
+                Spacer()
+            }
+        }
     }
 }
 
 struct PhotoGridView_Previews: PreviewProvider {
     static var previews: some View {
         PhotoGridView().environmentObject(AuthInfo())
+            .environmentObject(Settings())
     }
 }
